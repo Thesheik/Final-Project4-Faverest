546ebe0121fb1237dc9ee695df33ea46baf1b76f
Cheikh Ndiongue <Cmndiongue@yahoo.com>
2015-09-12 13:32:40 -0400

    Have all the crud functionality working.

--- faverest/app/controllers/favs_controller.rb
+++ faverest/app/controllers/favs_controller.rb
@@ -1,6 +1,50 @@
 class FavsController < ApplicationController
+  before_action :find_fav, only: [:show, :edit, :update, :destroy]
 
   def index
+    @favs = Fav.all.order("created_at DESC")
   end
-  
+
+  def show
+  end
+
+  def new
+    @fav = Fav.new
+  end
+
+  def create
+    @fav = Fav.new(fav_params)
+    if @fav.save
+      redirect_to @fav, notice: "Successfully created new fav"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @fav.update(fav_params)
+      redirect_to @fav, notice: "Fav was Successfully updated"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @fav.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def fav_params
+    params.require(:fav).permit(:title, :description)
+  end
+
+  def find_fav
+    @fav = Fav.find(params[:id])
+  end
+
 end
