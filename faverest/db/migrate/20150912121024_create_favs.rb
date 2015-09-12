b19ca0b12ae6280b865df40c8beca5c222f91eb9
Cheikh Ndiongue <Cmndiongue@yahoo.com>
2015-09-12 08:13:19 -0400

    app for favorites

---
+++ faverest/db/migrate/20150912121024_create_favs.rb
@@ -0,0 +1,10 @@
+class CreateFavs < ActiveRecord::Migration
+  def change
+    create_table :favs do |t|
+      t.string :title
+      t.text :description
+
+      t.timestamps null: false
+    end
+  end
+end
